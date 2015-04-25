f1fd000115c80f738a168a2c0e0568b755cd5515
Guillaume Burel <guillaume@yimgo.fr>
2015-04-25 23:31:23 +0200

    Get credentials from env

---
+++ app/config.js
@@ -0,0 +1,22 @@
+var config = Object.create(Object.prototype, {
+  CLIENT_ID: {
+    enumerable: true,
+    get: function () {
+      return process.env.SNDCLD_CLIENT_ID;
+    }
+  },
+  CLIENT_SECRET: {
+    enumerable: true,
+    get: function () {
+      return process.env.SNDCLD_CLIENT_SECRET;
+    }
+  },
+  OAUTH_TOKEN: {
+    enumerable: true,
+    get: function () {
+      return process.env.OAUTH_TOKEN;
+    }
+  }
+});
+
+module.exports = config;
