1667252d6fbdc63f56f5940e7e4ed87e7855d70e
Guillaume Burel <guillaume@yimgo.fr>
2015-04-26 17:16:32 +0200

    Bind strict mode in iife

--- app/app.js
+++ app/app.js
@@ -1,31 +1,32 @@
-'use strict';
+(function () {
+  'use strict';
+  var
+    express = require('express'),
+    sc = require('soundclouder'),
 
-var
-  express = require('express'),
-  sc = require('soundclouder'),
+    config = require('./config'),
+    User = require('./lib/user'),
 
-  config = require('./config'),
-  User = require('./lib/user'),
+    app = express(),
+    server;
 
-  app = express(),
-  server;
+  sc.init(config.CLIENT_ID, config.CLIENT_SECRET, 'http://localhost:3000/callback');
 
-sc.init(config.CLIENT_ID, config.CLIENT_SECRET, 'http://localhost:3000/callback');
+  app.get('/callback', function (req, res) {
+    var code = req.query.code;
+    console.log('Connection from user with code: %s', code);
+    User.create(code)
+      .then(function () {
+        res.redirect('/index.html');
+      }, function (err) {
+        console.error('Could not create user (%s)', err);
+        res.status(401).end();
+      });
+  });
 
-app.get('/callback', function (req, res) {
-  var code = req.query.code;
-  console.log('Connection from user with code: %s', code);
-  User.create(code)
-    .then(function () {
-      res.redirect('/index.html');
-    }, function (err) {
-      console.error('Could not create user (%s)', err);
-      res.status(401).end();
-    });
-});
+  app.use(express.static('app/assets'));
 
-app.use(express.static('app/assets'));
-
-server = app.listen(3000, function () {
-  console.log('App listening at http://%s:%s', server.address().address, server.address().port);
-});
+  server = app.listen(3000, function () {
+    console.log('App listening at http://%s:%s', server.address().address, server.address().port);
+  });
+}());
--- app/config.js
+++ app/config.js
@@ -1,24 +1,23 @@
-'use strict';
-
-var config = Object.create(Object.prototype, {
-  CLIENT_ID: {
-    enumerable: true,
-    get: function () {
-      return process.env.SNDCLD_CLIENT_ID;
+module.exports = (function () {
+  'use strict';
+  return Object.create(Object.prototype, {
+    CLIENT_ID: {
+      enumerable: true,
+      get: function () {
+        return process.env.SNDCLD_CLIENT_ID;
+      }
+    },
+    CLIENT_SECRET: {
+      enumerable: true,
+      get: function () {
+        return process.env.SNDCLD_CLIENT_SECRET;
+      }
+    },
+    OAUTH_TOKEN: {
+      enumerable: true,
+      get: function () {
+        return process.env.SNDCLD_OAUTH_TOKEN;
+      }
     }
-  },
-  CLIENT_SECRET: {
-    enumerable: true,
-    get: function () {
-      return process.env.SNDCLD_CLIENT_SECRET;
-    }
-  },
-  OAUTH_TOKEN: {
-    enumerable: true,
-    get: function () {
-      return process.env.SNDCLD_OAUTH_TOKEN;
-    }
-  }
-});
-
-module.exports = config;
+  });
+}());
